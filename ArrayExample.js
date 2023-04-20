8efcc106109a731089a5f13d2274ad326be5e3d0
stephback <stephanie.janeback@gmail.com>
2023-04-19 22:32:49 -0600

    Adding ArrayExample

---
+++ ArrayExample.js
@@ -0,0 +1,21 @@
+// Arrays are mutable in JS!?!
+
+// Declare an array called customerNames
+let customerNames = [];
+customerNames.push("Bob Loblaw");
+customerNames.push("Buster Bluth");
+
+// Can print by individual element index (position in array)
+console.log(customerNames[0]);
+
+// Or print all elements in array using a for loop i = index
+for (let i = 0; i < customerNames.length; i++) {
+  console.log(customerNames[i]);
+}
+
+// Enhanced for loop
+for(name.of customerNames){ // <-- Ask why name.of is deprecated
+    console.log(name);      // Week 3 Arrays 1st arrays video
+}
+
+
